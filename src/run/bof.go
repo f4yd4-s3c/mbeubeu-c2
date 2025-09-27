2700082e248971cff79ce85186ff594984c11691
P4p4 M4n3 <68352308+f4yd4-s3c@users.noreply.github.com>
2025-09-27 20:54:33 +0000

    We can use flags Now

--- src/run/bof.go
+++ src/run/bof.go
@@ -4,14 +4,33 @@ import (
 	"fmt"
 
 	"github.com/praetorian-inc/goffloader/src/coff"
+	"github.com/praetorian-inc/goffloader/src/lighthouse"
 )
 
-func BofExecute(bofByte, argsByte []byte) (result string) {
+
+func BofExecute(bofByte []byte, args []string) (result string) {
 	defer func() {
 		if r := recover(); r != nil {
 			result = fmt.Sprintf("Panic occurred when executing BOF: %v", r)
 		}
 	}()
+
+	// Build type string (e.g., "zzz" for 3 string args)
+	types := ""
+	for range args {
+		types += "z"
+	}
+
+	// Convert args to []interface{}
+	values := make([]interface{}, len(args))
+	for i, arg := range args {
+		values[i] = arg // Ensure values are interface{} types
+	}
+
+	argsByte, err := lighthouse.PackArgs(types, values)
+	if err != nil {
+		return fmt.Sprintf("Error packing arguments: %v", err)
+	}
 
 	output, err := coff.Load(bofByte, argsByte)
 	if err != nil {
@@ -20,3 +39,4 @@ func BofExecute(bofByte, argsByte []byte) (result string) {
 
 	return output
 }
+
